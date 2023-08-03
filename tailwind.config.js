5859ffde8ad0e480f0af6d1172c79d8db889670e
hanahana0201 <hanahana0201@gmail.com>
2023-08-03 16:38:40 +0900

    Add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontSize: {
+      xs: ['0.8rem', '1rem'],
+      sm: ['0.89rem', '1.25rem'],
+      base: ['1rem', '1.5rem'],
+      lg: ['1.14rem', '1.75rem'],
+      xl: ['1.33rem', '2rem'],
+      '2xl': ['1.6rem', '2.25rem'],
+      '3xl': ['2rem', '2.5rem'],
+      '4xl': ['2.67rem', '3.25rem'],
+      '5xl': ['4rem', '4.5rem'],
+      '6xl': ['8rem', '8rem'],
+    },
     extend: {
+      fontFamily: {
+        'roboto-mono': ['Roboto Mono', 'monospace'],
+      },
       spacing: {
         'gutter-x': 'max(2.5vw,1rem)',
         'gutter-y-0.5': 'max(1.25vw,1rem)',
